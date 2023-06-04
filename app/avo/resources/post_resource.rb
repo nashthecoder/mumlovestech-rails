86417abdecf77be136ec07a8a18d89472b260a11
nashthecoder <naijeria@gmail.com>
2023-06-04 13:59:02 +0300

    added avo admin template

---
+++ app/avo/resources/post_resource.rb
@@ -0,0 +1,14 @@
+class PostResource < Avo::BaseResource
+  self.title = :id
+  self.includes = []
+  # self.search_query = -> do
+  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  field :id, as: :id
+  # Fields generated from the model
+  field :title, as: :text
+  field :body, as: :textarea
+  field :tag, as: :text
+  # add fields here
+end
